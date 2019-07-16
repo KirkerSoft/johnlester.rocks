246c08e8b9ce96a74a33784e8e4e31d068bc7585
WSiaB <johnl@kirkersoft.com>
2019-07-15 20:24:08 -0700

    Moved resume to this site
    Moved portfolio to wsiab.net instead.

--- cst205/Lab2/tipAndBIll.py
+++
@@ -1,9 +0,0 @@
-meal = float(input("Enter cost of meal: "))
-tax = 0.0925
-tip = 0.15
-meal = meal + meal * tax
-total = meal + meal * tip
-print("$%.2f total" % total)
-diners = int(input("Enter number of diners: "))
-share = total / diners
-print("Each diners share is $%.2f" % share)
---
+++ index.php
@@ -0,0 +1,184 @@
+<html>
+<head>
+	<title>John Lester Résumé</title>
+	<style type=text/css>
+	* { font-family: Sans-serif; }
+	a, a:link, a:visited, a:active { color: black; text-decoration: none; }
+	a:hover { color: red; }
+	</style>
+</head>
+<body bgcolor=white>
+
+<table border=0 background="https://wsiab.net/media/global-ltr.jpg" cellpadding=5 cellspacing=5>
+	<tr>
+		<td width=450>
+		<td width=150>
+		<td width=150>
+		<td width=450>
+	</tr>
+	<tr>
+		<td colspan=4 align=center><h1>JOHN LESTER</h1>
+		<h2>INFORMATION TECHNOLOGY<br>
+		PROJECT MANAGER<br>
+		</h2></td>
+	</tr>
+	<tr>
+		<td><img src="hidden" width=1 height=150></td>
+		<td colspan=2 rowspan=2 align=center><img src="https://wsiab.net/media/circlePic.png"></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td><h3><a href="https://wsiab.net/media/commuteArea.png" target="_blank">Concord, California <img src="https://wsiab.net/media/Pin.png" align=right border=0></a></h3>
+		<hr>
+		<h3><a href="tel:+19252896969">+1 (925) 289 - 6969 <img src="https://wsiab.net/media/Phone.png" align=right></h3></a></td>
+		<td><h3><a href="contactform.htm">john<img src="https://wsiab.net/media/email_sign.png" border=0 width=15 height=15>johnlester.info <img src="https://wsiab.net/media/Letter.png" align=right border=0></a></h3>
+		<hr>
+		<h3><a href="http://www.johnlester.info" target="_top">http://www.JohnLester.info <img src="https://wsiab.net/media/URL.png" align=right border=0></a></h3></td>
+	</tr>
+	<tr>
+		<td colspan=2 bgcolor=white width=50% align=center><h3><br>
+		PROFILE</h3></td>
+		<td colspan=2 bgcolor=white width=50% align=center><h3><br>
+		EDUCATION</h3></td>
+	</tr>
+	<tr>
+		<td colspan=2 bgcolor="#EEEEEE" width=50%>Driven IT project manager with over 20 years of project management and information technology experience. Strengths include being highly adaptive; efficient with time management and multitasking; sensitive to project cost management; able to anticipate potential risks, plan for them and implement solutions; and able to coordinate multi-disciplinary teams effectively.</td>
+		<td colspan=2 bgcolor="#EEEEEE" width=50%>Bachelor of Science - Computer Science<br>
+		(California State University, Monterey Bay)<br clear=all>
+		<img src="https://wsiab.net/media/college.png" align=right><br>
+		Associate of Science - Computer Science<br>
+		(Diablo Valley College)<br>
+		Certificates in Computer Architecture, Advanced Program Design, and C++ Programming.</td>
+	</tr>
+	<tr>
+		<td colspan=2 bgcolor=white width=50% align=center><h3><br>
+		CERTIFICATIONS</h3></td>
+		<td colspan=2 bgcolor=white width=50% align=center><h3><br>
+		VOLUNTEERING</h3></td>
+	</tr>
+	<tr>
+		<td colspan=2 bgcolor="#EEEEEE" width=50%>Dell Certified Systems Expert (DCSE07), CompTIA A+, and currently working towards Project+ and PMI Project Management Professional (PMP).</td>
+		<td colspan=2 bgcolor="#EEEEEE" width=50%>First Aid/CPR/AED Trainer for Girl Scouts of Northern California<br>
+		Maker/Teacher Volunteer for Community Youth Center, Concord</td>
+	</tr>
+	<tr>
+		<td colspan=4 align=center bgcolor=white><h3><br>
+		EMPLOYMENT HISTORY</h3></td>
+	</tr>
+	<tr>
+		<td colspan=4 bgcolor="#EEEEEE">
+			<table border=0>
+				<tr>
+					<td align=center rowspan=2 valign=top>JUN 2016<br>
+					-<br>
+					FEB 2019</td>
+					<td colspan=2><b>SEPHORA US</b> (San Francisco, CA) / (Contract) - New Store IT Project Manager</td>
+				</tr>
+				<tr>
+					<td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+					<td>Responsible for the acquisition, preparation and deployment of all technologies related to the opening of new retail stores, as well as the relocations, expansions and remodels of existing stores. Acted as the liaison with multiple internal teams and multiple external service providers to support all technology aspects. Oversaw more than fifty $100k+ projects in 2018 alone. Developed processes to streamline project builds, implemented budgeting tools and integrated with vendor systems. Working as a team we were able to save Sephora over $600,000 in 2018 by replacing a key partner with one that was more agile, thereby obtaining better service for a much lower price.</td>
+				</tr>
+				<tr>
+					<td align=center rowspan=2 valign=top>MAR 2012<br>
+					-<br>
+					JAN 2016</td>
+					<td colspan=2><b>IMMUNOSCIENCE, INC</b> (Pleasanton, CA) - Facilities &amp; IT Manager</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td>Responsible for all computer and network security, administration and maintenance at five locations. Projects completed included:<ul>
+						<li> Implementation of a Cisco VPN solution to link offices and allow researchers in the field secure remote access to vital enterprise systems, 
+						<li> Rehabilitation of the company BSL3 lab, 
+						<li> Evaluated, purchased and implemented a Vaisala Veriteq validated continuous monitoring system for all xGMP lab equipment.</ul>
+						In addition, I assisted with technical writing of procedures and other documentation for xGMP compliance.</td>
+				</tr>
+				<tr>
+					<td align=center rowspan=2 valign=top>&nbsp;APR&nbsp;1998&nbsp;<br>
+					&#8209;<br>
+					MAR&nbsp;2012</td>
+					<td colspan=2><b>KIRKER ENTERPRISES</b> (Concord, CA) - Owner (Self-Employed)</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td>This entailed providing an array of IT project management and support services for companies of various sizes. Some of the larger companies included:<ul>
+						<li> Ball Metal Beverage Fairfield required end user support as well as regularly scheduled maintenance of their PC and network infrastructure in both their office and factory environments,
+						<li> Wincor Nixdorf required ATM hardware and software break/fix throughout Northern California,
+						<li>OnForce (contracts for AT&amp;T, Dell),
+						<li>TekSource (contracts for Office Depot, Williams Sonoma),
+						<li>ServiceLive (contracts for OfficeMax, Sears)
+						<li>Fujitsu, Xerox, Gurus2Go and many more.</ul></td>
+				</tr>
+				<tr>
+					<td align=center rowspan=2 valign=top>JUN 1991<br>
+					-<br>
+					OCT 1996</td>
+					<td colspan=2><b>US MARINE CORPS</b> (Hayward, CA) - HAWK Radar Repair Specialist</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td colspan=4 align=center bgcolor=white><h3><br>
+		SKILLS</h3></td>
+	</tr>
+	<tr>
+		<td colspan=4 bgcolor="#EEEEEE" align=center><img src="https://wsiab.net/media/skills.png" border=0 alt="Information Technology Project Manager, PC Setup, Server Setup, Network Setup, Long Term IT Planning, Agile and Waterfall Methodologies, Regulatory Complaince, Android, C++, Java, Software, CSS, HTML5, PHP, Web Development, PLC System Automation, Vendor Integration, Project Leadership, Vendor Negotiations, Stakeholder Communications, Risk Management, Cost Controls, Critical Thinking, Negotiations, Retail, Project Coordination"></td>
+	</tr>
+	<tr>
+		<td colspan=2 bgcolor=white width=50% align=center><h3><br>
+		OTHER INFORMATION</h3></td>
+		<td colspan=2 bgcolor=white width=50% align=center><h3><br>
+		INTERESTS</h3></td>
+	</tr>
+	<tr>
+		<td colspan=2 bgcolor="#EEEEEE">
+			<table border=0 width=100%>
+				<tr>
+					<td align=right width=40%><a href="https://www.linkedin.com/in/wsiab/" target=_blank>linkedin.com/in/wsiab <img src="https://wsiab.net/media/linkedin.png" align=right></a><br clear=all>
+					<br>
+					<a href="https://wsiab.net">wsiab.net (portfolio) <img src="https://wsiab.net/media/Home.png" align=right></a><br clear=all>
+					<br>
+					REFERENCES&nbsp;AVAILABLE <img src="https://wsiab.net/media/World.png" align=right><br>
+					UPON REQUEST </td>
+					<td width=10%> </td>
+					<td align=right width=40%><a href="https://www.youtube.com/c/JohnLesterRocks" target=_blank><img src="https://wsiab.net/media/YouTube.png" align=right> wsiab.net/youtube.php</a><br clear=all>
+					<br>
+					<a href="http://www.github.com/wsiab" target=_blank><img src="https://wsiab.net/media/Github.png" align=right> github.com/WSiaB</a><br clear=all>
+					<br>
+					<a href="skype:kirkersoft?chat"><img src="https://wsiab.net/media/skype.png" align=right> skype:kirkersoft</a></td>
+					<td width=10%> </td>
+				</tr>
+			</table>
+		</td>
+		<td colspan=2 bgcolor="#EEEEEE">
+			<table border=0 width=100%>
+				<tr>
+					<td align=right width=40%> Exploring <img src="https://wsiab.net/media/Exploring.png" align=right><br clear=all>
+					<br>
+					Gaming <img src="https://wsiab.net/media/Video-Games.png" align=right><br clear=all>
+					<br>
+					Motorcycling <img src="https://wsiab.net/media/Motorbikes.png" align=right></td>
+					<td width=10%> </td>
+					<td align=right width=40%><img src="https://wsiab.net/media/Walking.png" align=right> Outdoors <br clear=all>
+					<br>
+					<img src="https://wsiab.net/media/Computers.png" align=right> Programming <br clear=all>
+					<br>
+					<img src="https://wsiab.net/media/Boating.png" align=right> Sailing </td>
+					<td width=10%> </td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-3266499-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-3266499-1');
+</script>
+
+</body>
+</html>
